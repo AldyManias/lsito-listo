c170f3975df77f866904b2c4938c445d01d99735
AldyManias <aldim_24@hotmail.com>
2022-08-16 20:46:42 -0300

    tarea lista

--- app1.js
+++ app1.js
@@ -1,11 +1,11 @@
-// let Bienvenidos= "bienvenidos a nuestra pagina de rapipago";
-// console.log (Bienvenidos);
-// let Nombre= prompt("ingrese el su nombre y apellido");
-// console.log (Nombre);
-// let DNI= parseInt(prompt("ingrese su DNI" + "sin puntos ni espacios"))
-// console.log (DNI);
-// let norobot= parseInt (prompt("para asegurarnos que no es un robot indique cuanto es 9*9"));
-// console.log (norobot);
+let Bienvenidos= "bienvenidos a nuestra pagina de rapipago";
+console.log (Bienvenidos);
+let Nombre= prompt("ingrese el su nombre y apellido");
+console.log (Nombre);
+let DNI= parseInt(prompt("ingrese su DNI" + "sin puntos ni espacios"))
+console.log (DNI);
+let norobot= parseInt (prompt("para asegurarnos que no es un robot indique cuanto es 9*9"));
+console.log (norobot);
 let numero1= parseInt(prompt("ingrese un numero"));
 console.log (numero1)
 let numero2= parseInt(prompt("ingrese un numero"));
