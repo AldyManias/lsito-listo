fce4e0e99f0e88a8e8bde439244d033452889f98
AldyManias <aldim_24@hotmail.com>
2022-08-16 20:38:00 -0300

    listo listo

---
+++ app1.js
@@ -0,0 +1,17 @@
+// let Bienvenidos= "bienvenidos a nuestra pagina de rapipago";
+// console.log (Bienvenidos);
+// let Nombre= prompt("ingrese el su nombre y apellido");
+// console.log (Nombre);
+// let DNI= parseInt(prompt("ingrese su DNI" + "sin puntos ni espacios"))
+// console.log (DNI);
+// let norobot= parseInt (prompt("para asegurarnos que no es un robot indique cuanto es 9*9"));
+// console.log (norobot);
+let numero1= parseInt(prompt("ingrese un numero"));
+console.log (numero1)
+let numero2= parseInt(prompt("ingrese un numero"));
+console.log (numero2)
+let numero3= parseInt(prompt("ingrese un numero"));
+console.log (numero3)
+let resultado = parseInt(prompt("cuanto es: numero1 + numero2 + numero3"));
+console.log (resultado)
+
